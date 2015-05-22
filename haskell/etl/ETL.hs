be56239e2cd6ca92df47f831bcf2bb6b195d8b5b
Steven Tomcavage <stomcavage@weblinc.com>
2015-05-22 09:00:30 -0400

    Rewrite with foldr instead of foldl

--- haskell/etl/ETL.hs
+++ haskell/etl/ETL.hs
@@ -4,6 +4,6 @@ import Data.Char (toLower)
 import qualified Data.Map as M
 
 transform :: M.Map Int [String] -> M.Map String Int 
-transform inputMap = M.fromList $ foldl transform [] $ M.toList inputMap
-    where transform acc (key, vals) = [(map toLower v, key) | v <- vals] ++ acc
+transform inputMap = M.fromList $ foldr transform [] $ M.toList inputMap
+    where transform (key, vals) = (++) [(map toLower v, key) | v <- vals] 
 
