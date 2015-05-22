c56a04ac4b704fdf4db9d5855c4fe5061c72a697
Steven Tomcavage <stomcavage@weblinc.com>
2015-05-21 20:24:36 -0400

    ETL in Haskell

--- haskell/etl/ETL.hs
+++ haskell/etl/ETL.hs
@@ -1,7 +1,9 @@
 module ETL (transform) where
 
-import qualified Data.Map as Map
+import Data.Char (toLower)
+import qualified Data.Map as M
 
-transform :: Map.Map String Int -> Map.Map Int [String]
-transform = undefined
+transform :: M.Map Int [String] -> M.Map String Int 
+transform inputMap = M.fromList $ foldl transform [] $ M.toList inputMap
+    where transform acc (key, vals) = [(map toLower v, key) | v <- vals] ++ acc
 
